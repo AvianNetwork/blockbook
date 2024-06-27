3078be53180eb738f4cffb3c2ae034d01bdb3176
Craig Donnachie <craig.donnachie@gmail.com>
2024-06-27 12:17:23 -0400

    Add Avian

---
+++ bchain/coins/avian/avianparser.go
@@ -0,0 +1,75 @@
+package avian
+
+import (
+	"github.com/martinboehm/btcd/wire"
+	"github.com/martinboehm/btcutil/chaincfg"
+	"github.com/trezor/blockbook/bchain"
+	"github.com/trezor/blockbook/bchain/coins/btc"
+)
+
+// magic numbers
+const (
+	MainnetMagic wire.BitcoinNet = 0x4d4c5652
+	TestnetMagic wire.BitcoinNet = 0x544c5652
+)
+
+// chain parameters
+var (
+	MainNetParams chaincfg.Params
+	TestNetParams chaincfg.Params
+)
+
+func init() {
+	MainNetParams = chaincfg.MainNetParams
+	MainNetParams.Net = MainnetMagic
+	MainNetParams.PubKeyHashAddrID = []byte{60}
+	MainNetParams.ScriptHashAddrID = []byte{122}
+
+	TestNetParams = chaincfg.TestNet3Params
+	TestNetParams.Net = TestnetMagic
+	TestNetParams.PubKeyHashAddrID = []byte{111}
+	TestNetParams.ScriptHashAddrID = []byte{196}
+}
+
+// AvianParser handle
+type AvianParser struct {
+	*btc.BitcoinLikeParser
+	baseparser *bchain.BaseParser
+}
+
+// NewAvianParser returns new AvianParser instance
+func NewAvianParser(params *chaincfg.Params, c *btc.Configuration) *AvianParser {
+	return &AvianParser{
+		BitcoinLikeParser: btc.NewBitcoinLikeParser(params, c),
+		baseparser:        &bchain.BaseParser{},
+	}
+}
+
+// GetChainParams contains network parameters
+func GetChainParams(chain string) *chaincfg.Params {
+	if !chaincfg.IsRegistered(&MainNetParams) {
+		err := chaincfg.Register(&MainNetParams)
+		if err == nil {
+			err = chaincfg.Register(&TestNetParams)
+		}
+		if err != nil {
+			panic(err)
+		}
+	}
+	switch chain {
+	case "test":
+		return &TestNetParams
+	default:
+		return &MainNetParams
+	}
+}
+
+// PackTx packs transaction to byte array using protobuf
+func (p *AvianParser) PackTx(tx *bchain.Tx, height uint32, blockTime int64) ([]byte, error) {
+	return p.baseparser.PackTx(tx, height, blockTime)
+}
+
+// UnpackTx unpacks transaction from protobuf byte array
+func (p *AvianParser) UnpackTx(buf []byte) (*bchain.Tx, uint32, error) {
+	return p.baseparser.UnpackTx(buf)
+}
